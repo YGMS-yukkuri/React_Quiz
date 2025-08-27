780fe33934f651717980c930f57df673ab1ec3ea
yukkuri <reityan.sakura0104@gmail.com>
2025-08-27 19:51:26 +0900

    ボタン配置&Button.module.cssの名前ミスを修正

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button/Button";
 import Display from "../components/Display/Display"
 import quizData from "../data/quiz"
 
@@ -8,6 +9,11 @@ export default function QuizPage() {
             <Display>
                 {`Q1 ${quizData[quizIndex].question}`}
             </Display>
+            <Button>{quizData[quizIndex].options[0]}</Button>
+            <Button>{quizData[quizIndex].options[1]}</Button>
+            <Button>{quizData[quizIndex].options[2]}</Button>
+            <Button>{quizData[quizIndex].options[3]}</Button>
         </>
+
     )
 }
