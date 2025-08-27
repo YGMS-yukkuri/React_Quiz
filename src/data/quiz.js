8ef3e85b59937cd4a4a657f39b6e08633cfb3bb9
yukkuri <reityan.sakura0104@gmail.com>
2025-08-27 12:21:47 +0900

    クイズの内容をjsで配列に保存し、Inde番号に応じて読み込むように

---
+++ src/data/quiz.js
@@ -0,0 +1,29 @@
+const quizData = [
+    {
+        question: "Q1",
+        options: ["Select1", "Select2", "Select3", "Select4"],
+        answerIndex: 0
+    },
+    {
+        question: "Q2",
+        options: ["Select1", "Select2", "Select3", "Select4"],
+        answerIndex: 0
+    },
+    {
+        question: "Q3",
+        options: ["Select1", "Select2", "Select3", "Select4"],
+        answerIndex: 0
+    },
+    {
+        question: "Q4",
+        options: ["Select1", "Select2", "Select3", "Select4"],
+        answerIndex: 0
+    },
+    {
+        question: "Q5",
+        options: ["Select1", "Select2", "Select3", "Select4"],
+        answerIndex: 0
+    }
+];
+
+export default quizData;
--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,5 +1,13 @@
+import Display from "../components/Display/Display"
+import quizData from "../data/quiz"
+
 export default function QuizPage() {
-  return (
-    <div>QuizPage</div>
-  )
+    const quizIndex = 0;
+    return (
+        <>
+            <Display>
+                {`Q1 ${quizData[quizIndex].question}`}
+            </Display>
+        </>
+    )
 }
