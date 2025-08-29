2bd0931d8814788df3df6fb10c631f1575a70b03
yukkuri <reityan.sakura0104@gmail.com>
2025-08-29 18:43:03 +0900

    エフェクトの追加

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,18 +1,27 @@
 import { Link, useLocation } from "react-router-dom"
 import Result from "../components/Result/Result"
 import { ROUTES } from "../const"
+import Confetti from "react-confetti";
+import Loading from "../components/Loading/Loading";
+import { useEffect, useState } from "react";
 
 export default function ResultPage() {
+  const [active, setActive] = useState(false);
   const local = useLocation();
   const maxQuizLen = local.state.maxQuizLen;
   const correctNumLen = local.state.correctNumLen;
 
+  useEffect(() => {
+    setTimeout(() => setActive(true), 3000);
+  }, []);
   return (
     <>
-        <h1>Result</h1>
-        <Result maxQuizLen={maxQuizLen} correctNumLen={correctNumLen} />
-        <br/>
-        <Link to={ROUTES.QUIZ}>もう一度挑戦する</Link>
+      <Loading active={active} />
+      <h1>Result</h1>
+      <Result maxQuizLen={maxQuizLen} correctNumLen={correctNumLen} />
+      <br />
+      <Link to={ROUTES.QUIZ}>もう一度挑戦する</Link>
+      <Confetti />
     </>
   )
 }
