0b5e74f3892dc173771b209010e7d3be15e4ea3b
yukkuri <reityan.sakura0104@gmail.com>
2025-08-29 17:34:13 +0900

    結果ページの作成

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,5 +1,17 @@
+import { Link, useLocation } from "react-router-dom"
+
 export default function ResultPage() {
+  const local = useLocation();
+  const maxQuizLen = local.state.maxQuizLen;
+  const correctNumLen = local.state.correctNumLen;
+
   return (
-    <div>ResultPage</div>
+    <>
+        <h1>Result</h1>
+        <p>あなたの正解数は...</p>
+        <p>{`全${maxQuizLen}問中${correctNumLen}問正解でした!!!`}</p>
+        <br/>
+        <Link to={ROUTES.QUIZ}>もう一度挑戦する</Link>
+    </>
   )
 }
