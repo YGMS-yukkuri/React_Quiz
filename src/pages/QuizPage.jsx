fdd84ea2e7e7f3fc29bf3259a1d6c804338fde7b
yukkuri <reityan.sakura0104@gmail.com>
2025-08-29 16:07:12 +0900

    useEfectで完了検知を実装

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import Button from "../components/Button/Button";
 import Display from "../components/Display/Display"
 import quizData from "../data/quiz"
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../const";
 
 export default function QuizPage() {
     const [quizIndex, setQuizIndex] = useState(0);
     const [answerLogs, setAnswerLogs] = useState([]);
+    const navigation = useNavigate();
+    const MAX_QUIZ_LEN = quizData.length;
+
     const handleClick = (clickedIndex) => {
         if (clickedIndex === quizData[quizIndex].answerIndex) {
             setAnswerLogs((prev) => [...prev, true])
@@ -14,6 +19,21 @@ export default function QuizPage() {
         }
         setQuizIndex((prev) => prev + 1);
     }
+
+    useEffect(() => {
+        if(answerLogs.length === MAX_QUIZ_LEN){
+            const correctNum = answerLogs.filter((answer) => {
+                return answer === true;
+            })
+            navigation(ROUTES.RESULT, {
+                state: {
+                    maxQuizLen: MAX_QUIZ_LEN,
+                    correctNum: correctNum
+                }
+            });
+        }
+    },[answerLogs])
+    
     return (
         <>
             <Display>
